f6ea60197e205e1236b3dc01d55250f7e9943536
moyama55 <moyama04@gmail.com>
2022-01-23 13:02:01 +0000

    third commit

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,5 +1,26 @@
 class GenresController < ApplicationController
-    has_many :alchool_genres, dependent: :destroy
-    has_many :alchools, through: :alchool_genres
-    validates :name, presence: true
+　def index
+    @genre = Genre.new
+    @genres = Genre.all
+  end
+
+  def create
+    @genre = Genre.new(genre_params)
+    @genre.save
+  end
+
+  def edit
+    @genre = Genre.find(params[:id])
+  end
+
+  def update
+    @genre = Genre.find(params[:id])
+    @genre.update(genre_params)
+  end
+
+  private
+
+  def genre_params
+    params.require(:genre).permit(:name)
+  end
 end
